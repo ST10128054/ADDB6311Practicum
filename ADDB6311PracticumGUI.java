285a73dbf06db2b8c2a7a41251b9c7fbd648c95d
moozie <103250149+ST10128054@users.noreply.github.com>
2024-06-24 13:27:33 +0200

    Add files via upload
    Code for GUI

---
+++ ADDB6311PracticumGUI.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package addb6311practicumgui;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class ADDB6311PracticumGUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("--------------------------------------------------------");
+        System.out.println("The Adventure Diving Club(ADC) - REPORTING SYSTEM");
+        System.out.println("--------------------------------------------------------");
+        System.out.println("SELECT THE REQUIRED PRINT OUT: ");
+        System.out.println("1) Customer Details");
+        System.out.println("2) Total Dive Cost");
+        System.out.println("Enter report selection: ");
+    }
+    
+}
